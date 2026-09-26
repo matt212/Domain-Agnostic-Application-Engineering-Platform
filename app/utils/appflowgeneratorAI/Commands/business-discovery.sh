76c63a8bd4c46bf46c82a290dcf04a67fa11c1b5
Ritz212 <captainprice212@gmail.com>
2026-09-26 23:49:16 +0530

    business discovery updates

--- app/utils/appflowgeneratorAI/Commands/business-discovery.sh
+++ app/utils/appflowgeneratorAI/Commands/business-discovery.sh
@@ -2,7 +2,7 @@
 
 BUSINESS_IDEA="online groceries store"
 
-PROMPT="$(cat app/utils/appflowgeneratorAI/promptFile/business-discovery-prompt4.txt)"
+PROMPT="$(cat app/utils/appflowgeneratorAI/promptFile/business-discovery-prompt5.txt)"
 
 PROMPT="${PROMPT//\{\{BUSINESS_IDEA\}\}/$BUSINESS_IDEA}"
 PROMPT="${PROMPT//\{\{SEARCH_RESULTS\}\}/$SEARCH_RESULTS}"
@@ -11,4 +11,4 @@ llama-cli \
   -hf Qwen/Qwen3-8B-GGUF:Q4_K_M \
   -p "$PROMPT" \
   -n 10000 \
-  -o app/utils/appflowgeneratorAI/aiOutput/business-discovery-result-8b7.txt
+  -o app/utils/appflowgeneratorAI/aiOutput/business-discovery-result-8b8.txt
